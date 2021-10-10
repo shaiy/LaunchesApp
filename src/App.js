5b293a888573f2c23cd195cb26a539ffa95d3107
Shai Yomtov <shaiyomtov@Shais-MacBook-Pro-2.local>
2021-10-10 12:25:56 +0300

    removed more unneeded cra files + moved css to style.css file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import './App.css';
+import './style.css';
 
 const App = () => {
   return (
@@ -10,7 +10,7 @@ const App = () => {
         </Link>
         <Switch>
           <Route path="/launch/:id">
-            <div>Single launche page</div>
+            <div>Single launch page</div>
           </Route>
           <Route path="/">
             <div>Multiple launches page</div>
