17d166717298f934a76a0b4b2d349a3f4ed6c044
Shai Yomtov <shaiyomtov@Shais-MacBook-Pro-2.local>
2021-10-10 11:59:32 +0300

    added prettier config + removed cra not unneeded files + set up react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Link to="/">
+          <h1>Launches App</h1>
+        </Link>
+        <Switch>
+          <Route path="/launch/:id">
+            <div>Single launche page</div>
+          </Route>
+          <Route path="/">
+            <div>Multiple launches page</div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
