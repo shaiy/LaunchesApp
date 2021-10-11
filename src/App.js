7774d3174758774133d6d6a17db779578cf72a39
Shai Yomtov <shaiyomtov@Shais-MacBook-Pro-2.local>
2021-10-11 09:49:32 +0300

    configured tailwind + card design + launch indicator + share button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './style.css';
 
+import Launch from './Components/Launch';
+
 const App = () => {
   return (
     <div>
       <Router>
-        <Link to="/">
-          <h1>Launches App</h1>
-        </Link>
+        <header className="w-full mb-10 text-center p-7">
+          <Link to="/" className="text-6xl text-white hover:text-gray-200">
+            <h1>Launches App</h1>
+          </Link>
+        </header>
         <Switch>
           <Route path="/launch/:id">
-            <div>Single launch page</div>
+            <Launch
+              id={1234}
+              name={"Shai's launch"}
+              status={'success'}
+              missionDescription={
+                'This is the first flight of a small solid fuel rocket designed by iSpace, also known as Beijing Interstellar Glory Space Technology Ltd. It will carry several payloads for commercial customers to LEO.'
+              }
+              imageUrl="https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/launcher_images/new2520shepard_image_20190207032624.jpeg"
+              slug="slug"
+            />
           </Route>
           <Route path="/">
             <div>Multiple launches page</div>
