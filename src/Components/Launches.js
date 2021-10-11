ddd8e14f341ca0090f87b65d084ddbd24b8a916a
Shai Yomtov <shaiyomtov@Shais-MacBook-Pro-2.local>
2021-10-11 18:43:12 +0300

    future launches toggle

--- src/Components/Launches.js
+++ src/Components/Launches.js
@@ -1,21 +1,32 @@
 import { useState, useEffect } from 'react';
 import LaunchCard from './LaunchCard';
 import axios from 'axios';
+import Switch from 'react-switch';
 
 const Launches = () => {
-  //const [futureLaunches, setFutureLaunches] = useState(false);
-  const [launches, setLaunces] = useState([]);
+  const [futureLaunches, setFutureLaunches] = useState(false);
+  const [launches, setLaunches] = useState([]);
+
+  const handleFutureLaunches = () => {
+    setFutureLaunches(!futureLaunches);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios('https://lldev.thespacedevs.com/2.2.0/launch/upcoming/');
-      setLaunces(result.data.results);
+      const launchesType = futureLaunches ? 'upcoming' : 'previous';
+      const result = await axios(`https://lldev.thespacedevs.com/2.2.0/launch/${launchesType}/`);
+      setLaunches(result.data.results);
     };
     fetchData();
-  }, []);
+  }, [futureLaunches]);
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-auto">
+      <div className="flex mb-3 justify-center">
+        <span className="text-white	mr-2">Future Launches</span>
+        <Switch checked={futureLaunches} onChange={handleFutureLaunches} />
+      </div>
+
       {launches.map((launch) => (
         <div className="flex mb-3 justify-center" key={launch.id}>
           <LaunchCard
